dfaac5f46537c86123310d6a788c085f842d5a62
h8handles <kali@debian.home>
2020-09-27 22:09:03 -0400

    fixing for new builds, must switch zsh first

--- create.sh
+++ create.sh
@@ -13,6 +13,11 @@ sudo apt install terminator
 sudo apt install python3-pip
 
 #experimental!!!!!
+
+
+sudo apt install zsh
+
+chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
